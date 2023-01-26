48a98fc6d683cd609158d1c146d7cfdce264545b
Floha <floha2011@hotmail.de>
2023-01-26 18:59:35 +0100

    fix bot crashing when ceejus api not reachable, add proper error handling

--- src/api/apiFetcher.js
+++ src/api/apiFetcher.js
@@ -8,44 +8,50 @@ class ApiFetcher {
     }
     
     async handleMessage(args) {
-        let quote;
-        if (args.length > 0) {
-            const quoteNumber = parseInt(args[0], 10);
-            if (Number.isNaN(quoteNumber)) {
-                const response =
-                    await fetch(`${url}?alias=${args.join(' ')}`);
-                if (!response.ok) {
-                    return this.errorResponse(response);
+        try {
+            let quote;
+            if (args.length > 0) {
+                const quoteNumber = parseInt(args[0], 10);
+                if (Number.isNaN(quoteNumber)) {
+                    const response =
+                        await fetch(`${url}?alias=${args.join(' ')}`);
+                    if (!response.ok) {
+                        return this.errorResponse(response);
+                    } else {
+                        quote = await response.json();
+                    }
                 } else {
-                    quote = await response.json();
+                    const response =
+                        await fetch(`${url}?quoteNumber=${quoteNumber}`);
+                    if (!response.ok) {
+                        return this.errorResponse(response);
+                    } else {
+                        quote = await response.json();
+                    }
                 }
             } else {
                 const response =
-                    await fetch(`${url}?quoteNumber=${quoteNumber}`);
+                    await fetch(url);
                 if (!response.ok) {
                     return this.errorResponse(response);
                 } else {
                     quote = await response.json();
                 }
             }
-        } else {
-            const response =
-                await fetch(url);
-            if (!response.ok) {
-                return this.errorResponse(response);
-            } else {
-                quote = await response.json();
-            }
+            return quote;
+        } catch (error) {
+            return this.errorResponse({status: 'No Response'});
         }
-        return quote;
+        
     }
     
     errorResponse(response) {
         return {
             id: 0,
-            quote_text: 'An Error occured, please try again later! Error ' + response.status + ' #blameCJ',
-            quote_author: 'ERROR',
-            creation_date: '01/01/2000'
+            quote: 'An Error occured, please try again later! Error ' + response.status + ' #blameCJ',
+            quotedOn: 'ERROR',
+            creation_date: '01/01/2000',
+            quotedBy: 'none'
         };
     }
 }
